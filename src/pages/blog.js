af83eb20e8c57c64c55b0733e03c3f64ef3b7fee
Ra <58351832+rRatatoskr@users.noreply.github.com>
2023-11-27 16:43:42 +0900

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,26 +6,31 @@ import Seo from '../components/seo'
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle="我々の綴るところなのだ">
-            <ul>
-                {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
-                    ))
-                }
-            </ul>
+            {
+                data.allMdx.nodes.map((node) => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>{node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
 
 export const query = graphql`
-    query MyQuery {
-        allFile {
+    query {
+        allMdx(sort: {frontmatter: {date: DESC}}) {
             nodes {
-                name
+              frontmatter {
+                date(formatString: "YYYY.MM.DD")
+                title
+              }
+              id
+              excerpt
             }
-        }
+          }
     }
 `
 
